7a5912f0772b387e8a7fbd1c77b1f780d9158cb5
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2025-02-13 21:51:35 -0300

    build(deps): configure ESLint, Prettier, Husky and commitlint

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,14 @@
-import js from '@eslint/js'
-import globals from 'globals'
+import eslint from '@eslint/js'
+import eslintConfigPrettier from 'eslint-config-prettier'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    extends: [eslint.configs.recommended, ...tseslint.configs.recommended, eslintConfigPrettier],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -19,10 +19,31 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      camelcase: 'error',
+      'default-case': 'error',
+      'default-case-last': 'error',
+      'default-param-last': 'warn',
+      'dot-notation': 'error',
+      eqeqeq: 'error',
+      'max-classes-per-file': ['error', 1],
+      'new-cap': 'error',
+      'no-duplicate-imports': [
+        'error',
+        {
+          includeExports: true,
+        },
       ],
+      'no-else-return': 'error',
+      'no-empty-function': 'warn',
+      'no-eq-null': 'error',
+      'no-lonely-if': 'error',
+      'no-throw-literal': 'error',
+      'prefer-const': 'error',
+      'prefer-template': 'warn',
+      'require-await': 'error',
+      'sort-vars': 'warn',
+      yoda: 'warn',
     },
   },
 )
