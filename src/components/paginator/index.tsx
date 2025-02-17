935be474b1fac090d51225289074e279f6179e60
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2025-02-16 22:21:00 -0300

    feat(ui): create component for Pagination

---
+++ src/components/paginator/index.tsx
@@ -0,0 +1,38 @@
+import styles from './styles.module.css'
+
+export function Paginator() {
+  return (
+    <div className={styles.pagination}>
+      <div className={styles.paginationTotal}>Mostrando 2 de 100 usuários</div>
+      <div className={styles.paginator}>
+        <span className={styles.first}>&laquo;</span>
+        <a
+          href="#"
+          className={styles.prev}
+        >
+          &lsaquo;
+        </a>
+        {[1, 2, 3, 4].map((page) => (
+          <a
+            href="#"
+            key={page}
+          >
+            {page}
+          </a>
+        ))}
+        <a
+          href="#"
+          className={styles.next}
+        >
+          &rsaquo;
+        </a>
+        <a
+          href="#"
+          className={styles.last}
+        >
+          &raquo;
+        </a>
+      </div>
+    </div>
+  )
+}
