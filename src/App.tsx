6c6f0b0a40c59890613d0ed9cb4de0b9bcab0c10
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2025-02-17 19:02:35 -0300

    feat(ui): create modal to add a new user

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { Button } from '@components/button'
 import { Header } from '@components/header'
+import { InputGroup } from '@components/input-group'
+import { Modal } from '@components/modal'
+import { Paginator } from '@components/paginator'
 import { Sidebar } from '@components/sidebar'
 import { UserCard } from '@components/user-card'
-import { Paginator } from '@components/paginator'
+import { useState } from 'react'
 
 import styles from './styles.module.css'
 
@@ -49,6 +53,16 @@ const users = [
 ]
 
 export function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true)
+  }
+
+  function handleCloseModal() {
+    setIsModalOpen(false)
+  }
+
   return (
     <>
       <Header />
@@ -76,7 +90,12 @@ export function App() {
                 </button>
               </div>
 
-              <button className={styles.newEntry}>Criar usuário</button>
+              <Button
+                className={styles.newEntry}
+                onClick={handleOpenModal}
+              >
+                Criar usuário
+              </Button>
             </div>
 
             <div className={styles.filter}>
@@ -110,6 +129,186 @@ export function App() {
           <Paginator />
         </section>
       </main>
+
+      {isModalOpen && (
+        <Modal onClose={handleCloseModal}>
+          <div className={styles.modalCreateUser}>
+            <form className={styles.formCreateUser}>
+              <div className={styles.avatar}>
+                <img
+                  src="https://placehold.co/80x80"
+                  width={80}
+                  height={80}
+                  alt="Foto de perfil do novo usuário"
+                />
+
+                <InputGroup.Root>
+                  <InputGroup.Label htmlFor="avatar">Carregar avatar</InputGroup.Label>
+                  <InputGroup.Input
+                    type="file"
+                    inputId="avatar"
+                  />
+                </InputGroup.Root>
+              </div>
+
+              <InputGroup.Root>
+                <InputGroup.Label htmlFor="name">Nome</InputGroup.Label>
+                <InputGroup.Input
+                  inputId="name"
+                  placeholder="Nome completo"
+                />
+              </InputGroup.Root>
+
+              <InputGroup.Root>
+                <InputGroup.Label htmlFor="email">E-mail</InputGroup.Label>
+                <InputGroup.Input
+                  type="email"
+                  inputId="email"
+                  placeholder="E-mail do usuário"
+                />
+              </InputGroup.Root>
+
+              <InputGroup.Root>
+                <InputGroup.Label htmlFor="phone">Telefone</InputGroup.Label>
+                <InputGroup.Input
+                  type="tel"
+                  inputId="phone"
+                  placeholder="DDD + Número"
+                />
+              </InputGroup.Root>
+
+              <InputGroup.Root>
+                <InputGroup.Label htmlFor="birth">Data de nascimento</InputGroup.Label>
+                <InputGroup.Input
+                  type="text"
+                  inputId="birth"
+                  placeholder="DD/MM/AAAA"
+                />
+              </InputGroup.Root>
+
+              <InputGroup.Root>
+                <InputGroup.Label htmlFor="genre">Gênero</InputGroup.Label>
+                <InputGroup.Select inputId="genre">
+                  <option
+                    selected
+                    disabled
+                  >
+                    Selecionar gênero
+                  </option>
+                  <option value="male">Masculino</option>
+                  <option value="female">Feminino</option>
+                  <option value="non-binary">Não binário</option>
+                  <option value="none">Prefiro não informar</option>
+                </InputGroup.Select>
+              </InputGroup.Root>
+
+              <InputGroup.Root>
+                <InputGroup.Label htmlFor="zipcode">CEP</InputGroup.Label>
+                <InputGroup.Input
+                  type="text"
+                  inputId="zipcode"
+                  placeholder="00000-000"
+                />
+              </InputGroup.Root>
+
+              <div className={styles.twoColumns}>
+                <InputGroup.Root>
+                  <InputGroup.Label htmlFor="street">Rua</InputGroup.Label>
+                  <InputGroup.Input
+                    type="text"
+                    inputId="street"
+                    placeholder="Rua"
+                  />
+                </InputGroup.Root>
+
+                <InputGroup.Root>
+                  <InputGroup.Label htmlFor="street_number">Número</InputGroup.Label>
+                  <InputGroup.Input
+                    type="text"
+                    inputId="street_number"
+                    placeholder="Número"
+                  />
+                </InputGroup.Root>
+              </div>
+
+              <div className={styles.twoColumns}>
+                <InputGroup.Root>
+                  <InputGroup.Label htmlFor="complement">Complemento</InputGroup.Label>
+                  <InputGroup.Input
+                    type="text"
+                    inputId="complement"
+                    placeholder="Complemento"
+                  />
+                </InputGroup.Root>
+
+                <InputGroup.Root>
+                  <InputGroup.Label htmlFor="neighborhood">Bairro</InputGroup.Label>
+                  <InputGroup.Input
+                    type="text"
+                    inputId="neighborhood"
+                    placeholder="Bairro"
+                  />
+                </InputGroup.Root>
+              </div>
+
+              <div className={styles.twoColumns}>
+                <InputGroup.Root>
+                  <InputGroup.Label htmlFor="city">Cidade</InputGroup.Label>
+                  <InputGroup.Input
+                    type="text"
+                    inputId="city"
+                    placeholder="Cidade"
+                  />
+                </InputGroup.Root>
+
+                <InputGroup.Root>
+                  <InputGroup.Label htmlFor="state">Estado</InputGroup.Label>
+                  <InputGroup.Select inputId="state">
+                    <option
+                      selected
+                      disabled
+                    >
+                      Selecionar estado
+                    </option>
+                    <option value="AC">Acre</option>
+                    <option value="AL">Alagoas</option>
+                    <option value="AP">Amapá</option>
+                    <option value="AM">Amazonas</option>
+                    <option value="BA">Bahia</option>
+                    <option value="CE">Ceará</option>
+                    <option value="DF">Distrito Federal</option>
+                    <option value="ES">Espírito Santo</option>
+                    <option value="GO">Goiás</option>
+                    <option value="MA">Maranhão</option>
+                    <option value="MT">Mato Grosso</option>
+                    <option value="MS">Mato Grosso do Sul</option>
+                    <option value="MG">Minas Gerais</option>
+                    <option value="PA">Pará</option>
+                    <option value="PB">Paraíba</option>
+                    <option value="PR">Paraná</option>
+                    <option value="PE">Pernambuco</option>
+                    <option value="PI">Piauí</option>
+                    <option value="RJ">Rio de Janeiro</option>
+                    <option value="RN">Rio Grande do Norte</option>
+                    <option value="RS">Rio Grande do Sul</option>
+                    <option value="RO">Rondônia</option>
+                    <option value="RR">Roraima</option>
+                    <option value="SC">Santa Catarina</option>
+                    <option value="SP">São Paulo</option>
+                    <option value="SE">Sergipe</option>
+                    <option value="TO">Tocantins</option>
+                  </InputGroup.Select>
+                </InputGroup.Root>
+              </div>
+            </form>
+          </div>
+
+          <div className={styles.modalCreateUser__Footer}>
+            <Button onClick={handleCloseModal}>Cancelar</Button>
+            <Button type="submit">Salvar</Button>
+          </div>
+        </Modal>
+      )}
     </>
   )
 }
