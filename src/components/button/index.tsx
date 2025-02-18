fd35e7992e701f56a4783f6fea57c02186c3d570
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2025-02-17 23:15:50 -0300

    feat(ui): create appearance variant for Button component

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -2,12 +2,15 @@ import styles from './styles.module.css'
 
 type ButtonProps = {
   children: React.ReactNode
+  appearance?: 'primary' | 'secondary'
 } & React.ComponentProps<'button'>
 
-export function Button({ children, ...props }: ButtonProps) {
+export function Button({ children, appearance = 'primary', ...props }: ButtonProps) {
+  const appearanceClass = appearance === 'primary' ? styles.primaryButton : styles.secondaryButton
+
   return (
     <button
-      className={styles.button}
+      className={`${styles.button} ${appearanceClass}`}
       {...props}
     >
       {children}
